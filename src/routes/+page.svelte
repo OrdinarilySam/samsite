2fb75560470c7cbf168d0a489304f55464b3c872
Sam Martin <mart6353@umn.edu>
2024-05-07 14:36:41 -0500

    switched to ts and added blinking cursor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,76 +1,140 @@
-<script>
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import { onMount } from 'svelte';
+	import { spring } from 'svelte/motion';
+
+  const fullPos = 0.35;
+  const halfPos = 0.2;
+  const blinkInterval = 375;
+  const blinkTransition = 0.15;
+
+	let aboutDiv: HTMLElement;
+	let codeDiv: HTMLElement;
+	let photosDiv: HTMLElement;
+  let timeout: number = NaN;
+
+	let isVisible: boolean;
+	let isRunning: boolean = false;
+
+	let cursor = spring(
+		{ x: 0, y: 0 },
+		{
+			stiffness: 0.07,
+			damping: 0.45
+		}
+	);
+
+	function moveCursor(x: number, y: number) {
+    if (timeout) clearTimeout(timeout);
+		cursor.set({ x: x, y: y });
+		isVisible = true;
+		isRunning = false;
+		timeout = setTimeout(() => {
+			isRunning = true;
+      timeout = NaN;
+		}, 500);
+	}
+
+	function handleEnter(event: MouseEvent, fromLeft: number) {
+		if (event == null || event.target == null) return;
+		const target = event.target as HTMLElement;
+		const height = target.offsetHeight;
+		const width = target.offsetWidth;
+		const x = target.offsetLeft + width * fromLeft;
+		const y = target.offsetTop + height / 2;
+		moveCursor(x, y);
+	}
+
+	onMount(() => {
+		let height = aboutDiv.offsetHeight;
+		let width = aboutDiv.offsetWidth;
+		let y = aboutDiv.offsetTop + height / 2;
+		let x = aboutDiv.offsetLeft + width / 2;
+		moveCursor(x, y);
+		isRunning = true;
+	});
+
+	setInterval(() => {
+		isVisible = isRunning ? !isVisible : isVisible;
+	}, blinkInterval);
 </script>
 
 <svelte:head>
-  <title>Home</title>
-  <meta name="samsite" content="Svelte demo app" />
+	<title>Home</title>
+	<meta name="samsite" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-  <div class="about-container">
-    <p>Hi, <span class="about text">I'm Sam</span></p>
-  </div>
+	<a
+		class="about-container"
+		bind:this={aboutDiv}
+		on:mouseenter={(event) => handleEnter(event, fullPos)}
+    href="/about"
+	>
+    <p>Hi,</p>
+    <p>'m Sam</p>
+  </a>
 
-  <div class="code-container">
-    <p class="code text half">write code</p>
-  </div>
+	<a
+		class="code-container"
+		bind:this={codeDiv}
+		on:mouseenter={(event) => handleEnter(event, halfPos)}
+    href="/code"
+	>
+    <p>Write Code</p>
+  </a>
 
-  <div class="photos-container">
-    <p class="photos text half">take photos</p>
-  </div>
-
+	<a
+		class="photos-container"
+		bind:this={photosDiv}
+		on:mouseenter={(event) => handleEnter(event, halfPos)}
+    href="/photos"
+	>
+    <p>Take Photos</p>
+  </a>
+	<Icon
+		icon="mdi:cursor-text"
+		width="4em"
+		height="4em"
+		style="
+      position: absolute; 
+      pointer-events: none; 
+      top: {$cursor.y - 128}px; 
+      left: {$cursor.x - 128}px;
+      opacity: {isVisible ? '1' : '0'};
+      transition: opacity {blinkTransition}s;"
+	/>
 </section>
 
 <style>
-  section {
-    display: grid;
-    gap: 1rem;
-    grid-template-rows: 1fr 1fr;
-    grid-template-columns: 1fr 1fr;
-    height: 100%;
-    width: 100%;
-    font-size: 3rem;
-  }
+	section {
+		display: grid;
+		gap: 1rem;
+		grid-template-rows: 1fr 1fr;
+		grid-template-columns: 1fr 1fr;
+		height: 100%;
+		width: 100%;
+		font-size: 4rem;
+	}
 
-  section > div {
-    background-color: #fefefe;
-    text-align: center;
-    height: 100%;
-    position: relative;
-  }
+	section > a {
+		background-color: #fefefe;
+		text-align: center;
+		height: 100%;
+		position: relative;
+    text-decoration: none;
+    color: black;
+	}
 
-  div > p {
-    margin: auto;
-  }
+	.about-container {
+		grid-column: 1 / -1;
+	}
 
-  .about-container {
-    grid-column: 1 / -1;
-    display: flex;
-    flex-dir: row;
-    justify-content: center;
-  }
+	.code-container {
+		grid-column: 1 / 2;
+	}
 
-  .about-container > p {
-    width: 100%;
-  }
-
-  .code-container {
-    grid-column: 1 / 2;
-  }
-
-  .photos-container {
-    grid-column: 2 / 3;
-  }
-
-
-  .half {
-    position: absolute;
-    right: 0;
-    top: 50%;
-    transform: translate(0%, -50%);
-    width: 50%;
-  }
-
+	.photos-container {
+		grid-column: 2 / 3;
+	}
 </style>
-
-
--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -6,16 +6,16 @@
 
 <nav>
   <a href="/">
-    <Icon icon="mdi:home" width="50px" height="50px" />
+    <Icon icon="mdi:home" width="100%" height="100%" />
   </a>
   <a href="/about">
-    <Icon icon="mdi:account" width="50px" height="50px" />
+    <Icon icon="mdi:account" width="100%" height="100%" />
   </a>
   <a href="/code">
-    <Icon icon="mdi:code" width="50px" height="50px" />
+    <Icon icon="mdi:code" width="100%" height="100%" />
   </a>
   <a href="/photo">
-    <Icon icon="mdi:camera" width="50px" height="50px" />
+    <Icon icon="mdi:camera" width="100%" height="100%" />
   </a>
 
 </nav>
@@ -26,13 +26,16 @@
     flex-direction: column;
     background-color: var(--color-bg-1);
     justify-content: space-around;
-    padding: 1em;
+    padding: 1.5em;
     border-right: 2px solid black;
+    width: 11%;
   }
 
   a {
     text-decoration: none;
     color: black;
+    /* padding: 1.5em; */
+    margin: 1.5em;
   }
   
 </style>
