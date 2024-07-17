464704e467a854d948856eda7985aab0a6d5ac5a
Sam Martin <mart6353@umn.edu>
2024-07-17 18:37:38 -0500

    image spins on click

--- src/routes/about/Bio.svelte
+++ src/routes/about/Bio.svelte
@@ -1,42 +1,53 @@
 <script lang="ts">
 	import Headshot from '$lib/images/Headshot.jpeg';
-	import { preprocess } from 'svelte/compiler';
+	let polaroid: HTMLDivElement;
+	const baseRotation = 10;
+	let rotateLeft = true;
+	function rotatePolaroid() {
+		let rotation = rotateLeft ? 360 + baseRotation : baseRotation;
+		polaroid.style.transform = `rotate(${rotation}deg)`;
+		rotateLeft = !rotateLeft;
+	}
 </script>
 
 <div class="container">
 	<div class="image">
-    <div class="headshot-container">
-		  <img class="headshot" src={Headshot} alt="sam's headshot" />
-    </div>
+		<div
+			class="headshot-container"
+			bind:this={polaroid}
+			on:click={rotatePolaroid}
+			role="button"
+			aria-label="spins the headshot"
+			tabindex="0"
+			on:keydown={rotatePolaroid}
+		>
+			<img class="headshot" src={Headshot} alt="sam's headshot" />
+		</div>
 		<img
 			class="pin"
 			src="http://www.clker.com/cliparts/B/V/P/X/Z/e/thumbtack-pushpin-2-th.png"
 			alt="Thumbtack/pushpin 2 clip art"
 		/>
 	</div>
-  <div class="socials">
-
-  </div>
-  <div class="text">
-    <h1>
-      Sam Martin
-    </h1>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci maxime inventore neque quis
-      quaerat ex autem natus non sunt? Provident, cupiditate explicabo totam velit necessitatibus quos
-      repellendus autem perspiciatis! Temporibus. Neque dignissimos illum aperiam nam libero impedit
-      temporibus deserunt? Dolores corrupti voluptatibus eveniet velit id nulla, consequuntur adipisci
-      dolore quaerat rerum nostrum tenetur sed maiores voluptatum placeat provident, praesentium aut!
-      Reiciendis explicabo provident impedit necessitatibus ipsum odio? Repellat voluptas nihil enim
-      dolore adipisci libero dolorem iure suscipit. Pariatur ea enim ipsa! Eligendi accusantium
-      pariatur, placeat ea ipsum repellendus cum laboriosam. Recusandae a fugiat minima labore
-      veritatis, sit eum veniam ut voluptatibus quia corrupti ipsum earum exercitationem praesentium
-      modi ipsa ratione. Neque et alias nisi corrupti eos provident qui fugiat velit. In corporis
-      aspernatur possimus perferendis iure dolorum! Debitis vitae eligendi cumque. Deleniti doloremque
-      aspernatur debitis magnam alias soluta animi, quisquam culpa voluptatem assumenda, iusto id nisi
-      maxime illum corrupti rerum.
-    </p>
-  </div>
+	<div class="socials"></div>
+	<div class="text">
+		<h1>Sam Martin</h1>
+		<p>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci maxime inventore neque quis
+			quaerat ex autem natus non sunt? Provident, cupiditate explicabo totam velit necessitatibus
+			quos repellendus autem perspiciatis! Temporibus. Neque dignissimos illum aperiam nam libero
+			impedit temporibus deserunt? Dolores corrupti voluptatibus eveniet velit id nulla,
+			consequuntur adipisci dolore quaerat rerum nostrum tenetur sed maiores voluptatum placeat
+			provident, praesentium aut! Reiciendis explicabo provident impedit necessitatibus ipsum odio?
+			Repellat voluptas nihil enim dolore adipisci libero dolorem iure suscipit. Pariatur ea enim
+			ipsa! Eligendi accusantium pariatur, placeat ea ipsum repellendus cum laboriosam. Recusandae a
+			fugiat minima labore veritatis, sit eum veniam ut voluptatibus quia corrupti ipsum earum
+			exercitationem praesentium modi ipsa ratione. Neque et alias nisi corrupti eos provident qui
+			fugiat velit. In corporis aspernatur possimus perferendis iure dolorum! Debitis vitae eligendi
+			cumque. Deleniti doloremque aspernatur debitis magnam alias soluta animi, quisquam culpa
+			voluptatem assumenda, iusto id nisi maxime illum corrupti rerum.
+		</p>
+	</div>
 </div>
 
 <style>
@@ -50,11 +61,11 @@
 
 	.image {
 		position: relative;
-    grid-area: picture;
+		grid-area: picture;
 	}
 
-  .headshot-container {
-    position: absolute;
+	.headshot-container {
+		position: absolute;
 		top: 1.5rem;
 		right: 3rem;
 		transform: rotate(10deg);
@@ -63,32 +74,33 @@
 		border: 1px solid black;
 		background: white;
 		padding: 13px;
-  }
+		transition: transform 0.75s ease-in-out;
+	}
 
 	.headshot {
 		border: 1px inset black;
 		width: 100%;
 	}
 
-  .container {
-    grid-template-areas: "text picture" "text socials";
-    grid-template-columns: 3fr 2fr;
-    grid-template-rows: 1fr 1fr;
-    display: grid;
-    height: 100%;
-    width: 100%;
-  }
+	.container {
+		grid-template-areas: 'text picture' 'text socials';
+		grid-template-columns: 3fr 2fr;
+		grid-template-rows: 1fr 1fr;
+		display: grid;
+		height: 100%;
+		width: 100%;
+	}
 
-  .text {
-    grid-area: text;
-    padding: 0.75rem;
-  }
+	.text {
+		grid-area: text;
+		padding: 0.75rem;
+	}
 
-  .text > h1 {
-    font-size: 2rem;
-  }
+	.text > h1 {
+		font-size: 2rem;
+	}
 
-  .socials {
-    grid-area: socials;
-  }
+	.socials {
+		grid-area: socials;
+	}
 </style>
