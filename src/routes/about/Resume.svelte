4eaf6c1ecb58ad5b0953a931dfcf9a74948acea2
ordinarilysam <ordinarilysam@gmail.com>
2024-07-30 20:03:40 -0500

    experience section - resume

--- src/routes/about/Resume.svelte
+++ src/routes/about/Resume.svelte
@@ -5,12 +5,11 @@
 	let resume: ResumeT = {} as ResumeT;
 
 	onMount(() => {
-    async function loadResume(){
-		  resume = await getResume();
-    }
-    loadResume();
+		async function loadResume() {
+			resume = await getResume();
+		}
+		loadResume();
 	});
-
 </script>
 
 <div id="printed">
@@ -27,37 +26,52 @@
 			</div>
 		</section>
 		<section>
-			<h2 class="left title">Education</h2>
+			<h2 class="title">Education</h2>
 			<hr />
 			{#each resume.Education as edu}
-				<div class="container">
-					<p class="left">{edu.location}</p>
-					<h3 class="center">{edu.school}</h3>
-					<p class="right">{edu['start-date']} - {edu['end-date']}</p>
+				<div class="container canonical">
+					<p>{edu.location}</p>
+					<h3>{edu.school}</h3>
+					<p>{edu['start-date']} - {edu['end-date']}</p>
 				</div>
 				<p class="left">{edu.degree}</p>
 				<p class="left"><em>Cumulative GPA: </em>{edu.gpa}</p>
 				<p class="left"><em>Honors: </em>{edu.honors}</p>
 			{/each}
 		</section>
-    <section>
-      <h2 class="left title">Skills</h2>
-      <hr />
-      <div class="container">
-        {#each Object.entries(resume.Skills) as [skill, skills]}
-        <div>
-          <h3 class="center">{skill}</h3>
-          <p class="center">{skills.join(", ")}</p>
-        </div>
-        {/each}
-      </div>
-    </section>
+		<section>
+			<h2 class="title">Skills</h2>
+			<hr />
+			<div class="container">
+				{#each Object.entries(resume.Skills) as [skill, skills]}
+					<div>
+						<h3 class="center">{skill}</h3>
+						<p class="center">{skills.join(', ')}</p>
+					</div>
+				{/each}
+			</div>
+		</section>
+		<section>
+			<h2 class="title">Experience</h2>
+			<hr />
+			{#each resume.Experience as exp}
+				<div class="container canonical">
+					<p>{exp.position}</p>
+					<h3>{exp.company}</h3>
+					<p>{exp['start-date']} - {exp['end-date']}</p>
+				</div>
+        <ul class="list-items">
+          {#each exp.about as about}
+            <li>{about}</li>
+          {/each}
+        </ul>
+			{/each}
+		</section>
 	{/if}
 </div>
 
-
 <style>
-  #printed {
+	#printed {
 		font-family: 'Times New Roman', Times, serif;
 	}
 
@@ -65,15 +79,32 @@
 		text-align: center;
 	}
 
-  .about-container {
-    justify-content: space-between;
-    width: 100%;
-    display: flex;
-  }
+	.about-container {
+		justify-content: space-between;
+		width: 100%;
+		display: flex;
+	}
 
 	.container {
 		display: grid;
 		grid: 1fr / 1fr 1fr 1fr;
+	}
+
+	.container.canonical > *:nth-child(1),
+	.left {
+		text-align: left;
+	}
+	.container.canonical > *:nth-child(2),
+	.center {
+		text-align: center;
+	}
+	.container.canonical > *:nth-child(3),
+	.right {
+		text-align: right;
+	}
+
+	.container.canonical > * {
+		margin: 0.25rem 0;
 	}
 
 	.name {
@@ -87,24 +118,7 @@
 		margin-top: 0;
 		font-size: 1.25rem;
 		font-weight: bold;
-	}
-
-	.left {
 		text-align: left;
-	}
-
-	.right {
-		text-align: right;
-	}
-
-	.center {
-		text-align: center;
-	}
-
-	.left,
-	.center,
-	.right {
-		margin: 0.25rem 0;
 	}
 
 	em {
@@ -112,29 +126,38 @@
 		font-weight: bold;
 	}
 
-  section {
-    margin-bottom: 1rem;
+	section {
+		margin-bottom: 1rem;
+	}
+
+  .list-items {
+    width: 100%;
+    text-align: left;
   }
 
-  @media print {
-    @page {
-      margin: 0;
-    }
-
-    #printed {
-      padding: 2rem;
-      display: block;
-      position: absolute;
-      top: 0;
-      left: 0;
-      height: 100vh;
-      width: 100vw;
-      background-color: white;
-      color: black;
-    }
-
-    #printed h1 {
-      font-size: 2rem;
-    }
+  .list-items > * {
+    width: 100%;
   }
+
+	@media print {
+		@page {
+			margin: 0;
+		}
+
+		#printed {
+			padding: 2rem;
+			display: block;
+			position: absolute;
+			top: 0;
+			left: 0;
+			height: 100vh;
+			width: 100vw;
+			background-color: white;
+			color: black;
+		}
+
+		#printed h1 {
+			font-size: 2rem;
+		}
+	}
 </style>
